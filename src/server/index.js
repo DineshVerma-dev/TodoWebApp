5c661cb550a58899ba91da203f780ad7821a9823
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2024-08-17 15:26:15 +0530

    done the port

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 
 const app =  express();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000 || todowebapp-livid.vercel.app;
 
 Connection();
 
